883273ab8a31e098325710d49257d8fb3aae768b
mhj1205 <lightnow1205@gmail.com>
2024-09-02 16:14:47 +0900

    day01

---
+++ 20240828JAVA/java_work01/day01proj/day01proj/src/org/comstudy/App.java
@@ -0,0 +1,14 @@
+package org.comstudy;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // 변수 선언 : 타입 변수명 = 초기값;
+        // ---- 논리형 boolean
+        // ---- 실수형 float, double (기본 double)
+        // ---- 정수형 byte, short, int, long (기본 int)
+        // ---- 문수형 char (문자형도 정수형에 속한다. 아스키코드 라서)
+        String greeting = "안녕 세계";
+
+        System.out.println(greeting);
+    }
+}
